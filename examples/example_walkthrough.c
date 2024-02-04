9431d162b93b7d46392c09af15cd5713bd81c944
LeahChi <146640958+LeahChi@users.noreply.github.com>
2024-02-04 12:11:02 +0000

    cmd line comments

--- examples/example_walkthrough.c
+++ examples/example_walkthrough.c
@@ -11,20 +11,25 @@ int main(int argc, char *argv[])
 {
 
     if (argc != 3)
-    // user has to input 3 arguments 
+    // user has to input 3 arguments;
+    // 1-> ./ "name of file to run" 2-> name 3-> age
     {
         printf("Usage: wlk <name>\n");
         return 0;
     }
 
     int age = atoi(argv[2]);
-    if (age < 1)
+    // 'atoi' converting char into int
+    // atoi returns 0 if theres an error; <- important, therefore easy to error check
+    // age is collected through indexing
+    if (age < 1) // if invalid input or if user puts an age < 1
     {
         printf("Error: invalid age\n");
         return 1;
     }
 
     printf("Hello, %s! You are %d years old :)\n", argv[1], age);
+    //argv[1] is the 2nd arg -> name (any string)
 
     return 0;
 }
